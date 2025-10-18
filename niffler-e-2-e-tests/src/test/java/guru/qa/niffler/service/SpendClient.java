ea297e55086f2dd533e6b3a74a7b018ae92a19ef
Elena <e.sytina@ya.ru>
2025-10-18 16:02:05 +0300

    + refactor

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendClient.java
@@ -1,21 +1,19 @@
 package guru.qa.niffler.service;
 
-import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface SpendClient {
 
-  SpendJson createSpend(SpendJson spend);
+    SpendJson createSpend(SpendJson spend);
 
-  SpendJson getSpendById(UUID spendId, String userName);
+    SpendJson getSpendById(UUID spendId, String userName);
 
-  List<SpendJson> getAllSpends(String username);
+    List<SpendJson> getAllSpends(String username);
 
-  SpendJson updateSpend(SpendJson spend);
+    SpendJson updateSpend(SpendJson spend);
 
-  SpendJson deleteSpend(SpendJson spendJson);
+    SpendJson deleteSpend(SpendJson spendJson);
 }
