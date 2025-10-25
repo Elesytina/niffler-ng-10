12ced020e9f3b1be18babdfe062f520a2c6f57b0
Elena <e.sytina@ya.ru>
2025-10-25 22:27:26 +0300

    + LoginPage

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
@@ -2,17 +2,49 @@ package guru.qa.niffler.page;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
 
 public class LoginPage {
-  private final SelenideElement usernameInput = $("#username");
-  private final SelenideElement passwordInput = $("#password");
-  private final SelenideElement submitBtn = $("#login-button");
+    private final SelenideElement usernameInput = $("#username");
+    private final SelenideElement passwordInput = $("#password");
+    private final SelenideElement submitBtn = $("#login-button");
+    private final SelenideElement createNewBtn = $("#register-button");
 
-  public MainPage login(String username, String password) {
-    usernameInput.val(username);
-    passwordInput.val(password);
-    submitBtn.click();
-    return new MainPage();
-  }
+    public MainPage login(String username, String password) {
+        inputUserName(username);
+        inputPassword(password);
+        submit();
+        return page(MainPage.class);
+    }
+
+    public LoginPage inputUserName(String username) {
+        usernameInput.sendKeys(username);
+        return this;
+    }
+
+    public LoginPage inputPassword(String password) {
+        passwordInput.sendKeys(password);
+        return this;
+    }
+
+    public LoginPage submit() {
+        submitBtn.click();
+        return this;
+    }
+
+    public RegistrationPage clickCreateAccount() {
+        createNewBtn.click();
+        return page(RegistrationPage.class);
+    }
+
+    //"Неверные учетные данные пользователя"
+    public LoginPage checkIncorrectCredsDataError(String message) {
+        var element = $(".form__error");
+        element.shouldBe(visible);
+        element.shouldHave(text(message));
+        return this;
+    }
 }
